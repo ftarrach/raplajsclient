321f9e22f722a072d111f4a7a64a155c3de53969
Fabian Tarrach <33595266+ftarrach@users.noreply.github.com>
2018-02-24 19:29:20 +0100

    drilldown responsitivity

--- src/ui/BDrilldown.vue
+++ src/ui/BDrilldown.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
   div.drilldown
-    div.drilldown-menu
-      b-button(icon="fa-home" no-text @click="home")
-      b-button(icon="fa-arrow-left" no-text pull-right @click="back")
+    div.drilldown-menu.columns.is-gapless.is-marginless
+      .column.is-half
+        b-button(icon="fa-home" no-text fill :disabled="notHome" @click="home")
+      .column.is-half
+        b-button(icon="fa-arrow-left" no-text fill :disabled="notHome" pull-right @click="back")
     ul.drilldown-list
       li.drilldown-list-item(v-for="item in itemsForPath" :key="item.id")
         span(v-if="item.children" @click="clickOnContainer(item)")
@@ -36,6 +38,9 @@ export default {
         level = level.find(i => i.id === part).children
       }
       return level
+    },
+    notHome() {
+      return this.path.length === 0
     }
   },
 
@@ -64,8 +69,19 @@ export default {
 <style lang="scss" scoped>
   @import '../assets/sass/bulma.scss';
 
+  .drilldown {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 auto;
+    min-height: 100%;
+  }
+
   .drilldown-list {
+    flex-grow: 1;
     border: 1px solid $border;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
 
     .drilldown-list-item {
       cursor: pointer;
@@ -74,7 +90,8 @@ export default {
         background-color: $white-bis;
       }
 
-      &:not(:last-child) {
+      &:not(:last-child),
+      &:only-child {
         border-bottom: 1px solid $border;
       }
     }
