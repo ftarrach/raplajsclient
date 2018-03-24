a2ddbc888e7a5f60cfa0ea052c524efdee0a43f3
Fabian Tarrach <33595266+ftarrach@users.noreply.github.com>
2018-03-24 18:44:33 +0100

    declare rxjs globally for rapla interop

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import router from './routes'
 import store from './stores/store'
 import LayoutHelper from './globalComponents'
 import RaplaGwtPlugin from './plugins/RaplaGwtPlugin'
+import Rx from 'rxjs'
 
 import 'cool-checkboxes-for-bulma.io/dist/css/bulma-radio-checkbox.min.css'
+
+window.Rx = Rx
 
 moment.locale('de')
 
